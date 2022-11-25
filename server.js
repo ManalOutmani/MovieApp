90ffc3aa5fecbcb5ccc596b8f3dbdd05d49ba3bf
Manal El Outmani <manal.eloutmani20@gmail.com>
2022-11-25 11:06:26 +0100

    updates to deploy to heroku4

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const path=require('path');
-const app=express()
-
-app.use('/',express.static(path.join(__dirname,'angular')));
-
-app.get('',(req,res)=>{
-  res.sendFile(path.join(__dirname,'filmsapp/src','index.html'));
-});
-
-/*app.listen(8080,()=>{
-  console.log('server running http://localhost:${8080}');
-});*/
-
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/movies_back/filmsapp/src'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/movies_back/filmsapp/src/index.html'));});
 app.listen(process.env.PORT || 8080);
+
